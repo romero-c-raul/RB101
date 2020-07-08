944efd01a2231c71e3bbddf3ea5db8e604ee719f
Raul Romero <raulromerocano@aggienetwork.com>
2020-07-08 22:42:43 +0000

    Refactored code per LS recommendations

--- lesson_2/16_mortgage_calc/mortgage_calc.rb
+++ lesson_2/16_mortgage_calc/mortgage_calc.rb
@@ -1,5 +1,15 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_messages.yml')
+
 def prompt(message)
   puts "=> #{message}"
+end
+
+def welcome_message
+  puts ""
+  prompt MESSAGES['welcome']
+  prompt MESSAGES['description']
+  puts ""
 end
 
 def valid_number?(num, type = 'float')
@@ -7,76 +17,141 @@ def valid_number?(num, type = 'float')
   single_digit.pop if type == 'integer'
 
   valid_integer = nil
+  integer_array = []
 
-  num.chars.each do |element|
-    valid_integer = single_digit.include?(element) ? true : false
+  num.chars.map do |element|
+    valid_integer = single_digit.include?(element)
+    integer_array.push(element) if valid_integer == true
     break if valid_integer == false
   end
+  valid_integer = false if integer_array.count(".") > 1
   valid_integer
 end
 
-prompt "-----Welcome to Amortized Loan Calculator!-----"
-prompt "Use this calculator for basic calculations of common loan types"
-puts ""
-
-loop do
-  prompt "What is the loan amount?"
-  loan_amount = nil
-
+def retrieve_loan_amount
+  prompt MESSAGES['loan_amount']
   loop do
     loan_amount = gets.chomp
     if valid_number?(loan_amount)
       loan_amount = loan_amount.to_f
-      break
+      return loan_amount unless loan_amount == 0
+      prompt MESSAGES['non_zero_error']
     else
-      prompt "Enter positive number. Please try again"
+      prompt MESSAGES['error']
     end
   end
+end
 
-  prompt "What is the APR (percentage)?"
-  apr = nil
-
+def retrieve_apr_amount
+  prompt MESSAGES['apr_amount']
   loop do
     apr = gets.chomp
     if valid_number?(apr)
       apr = apr.to_f
-      break
+      return apr
     else
-      prompt "Enter positive number. Please try again"
+      prompt MESSAGES['error']
     end
   end
+end
 
-  prompt "Indicate loan term (in months)"
-  monthly_term = nil
-
+def retrieve_duration
+  prompt MESSAGES['monthly_term_amount']
   loop do
     monthly_term = gets.chomp
     if valid_number?(monthly_term, 'integer')
       monthly_term = monthly_term.to_i
-      break
+      return monthly_term unless monthly_term == 0
+      prompt MESSAGES['non_zero_error']
     else
-      prompt "Enter positive number (no decimals). Please try again"
+      prompt MESSAGES['no_decimals_or_zero_error']
     end
   end
+end
 
-  monthly_interest = (apr / 12) / 100
+def calculate_monthly_interest(apr)
+  (apr / 12) / 100
+end
 
-  monthly_payments =
+def calculate_monthly_payments(loan_amount, monthly_interest, monthly_term)
+  if monthly_interest != 0
     loan_amount * (monthly_interest /
-                  (1 - (1 + monthly_interest)**-monthly_term))
+                    (1 - (1 + monthly_interest)**-monthly_term))
+  else
+    loan_amount / monthly_term
+  end
+end
 
-  total_paid = monthly_payments * monthly_term
-  total_interest = total_paid - loan_amount
+def calculate_total_paid(monthly_payments, monthly_term)
+  monthly_payments * monthly_term
+end
 
+def calculate_total_interest(total_paid, loan_amount)
+  total_paid - loan_amount
+end
+
+def display_loan_information(monthly_payments,
+                             total_paid,
+                             monthly_term,
+                             total_interest)
   prompt "Your payment every month: $#{monthly_payments.round(2)}"
   prompt "Total of #{monthly_term} payments: $#{total_paid.round(2)}"
   prompt "Total interest: $#{total_interest.round(2)}"
   puts "---------------------------------------------------------------"
+end
 
-  prompt "Would you like to calculate another loan? (Y/N)"
-  answer = gets.chomp
-  break if answer.downcase != 'y'
+def retrieve_new_calculation_answer
+  prompt MESSAGES['another_loan?']
+  acceptable_answers = ['y', 'n', 'yes', 'no']
+
+  loop do
+    answer = gets.chomp.downcase
+    if acceptable_answers.include?(answer)
+      return answer
+    else
+      puts MESSAGES['answer_error']
+    end
+  end
+end
+
+def play_again?(answer)
+  if answer.start_with?('y')
+    true
+  else
+    false
+  end
+end
+
+def display_goodbye
+  prompt('Goodbye!')
+  puts ""
+end
+
+def clear_terminal
+  system("clear")
   puts "---------------------------------------------------------------"
 end
 
-prompt('Goodbye!')
+welcome_message
+
+loop do
+  loan_amount = retrieve_loan_amount
+  apr = retrieve_apr_amount
+  monthly_term = retrieve_duration
+  monthly_interest = calculate_monthly_interest(apr)
+
+  monthly_payments =
+    calculate_monthly_payments(loan_amount, monthly_interest, monthly_term)
+  total_paid = calculate_total_paid(monthly_payments, monthly_term)
+  total_interest = calculate_total_interest(total_paid, loan_amount)
+
+  display_loan_information(monthly_payments,
+                           total_paid,
+                           monthly_term,
+                           total_interest)
+  answer = retrieve_new_calculation_answer
+  break unless play_again?(answer)
+
+  clear_terminal
+end
+display_goodbye
